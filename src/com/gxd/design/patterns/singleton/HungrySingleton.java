f13326617d1500c368457c443e81355c52e07a1f
gxd <guoxiaodong@haqu.com>
2020-01-30 12:04:38 +0800

    optimize:饿汉单例

--- src/com/gxd/design/patterns/singleton/HungrySingleton.java
+++ src/com/gxd/design/patterns/singleton/HungrySingleton.java
@@ -9,6 +9,14 @@ public class HungrySingleton {
     private HungrySingleton() {
     }
 
+    /**
+     * 当调用静态方法时,会初始化静态成员变量
+     * 如果单例只有一个静态方法getInstance(),则不存在提前初始化问题
+     */
+    public static void otherStaticMethod() {
+        System.out.println("hahaha");
+    }
+
     public static HungrySingleton getInstance() {
         return INSTANCE;
     }
